766781745d4ba917c01c853d6d3aba21cb45a9e0
Mehrad Rousta <mehrad77@gmail.com>
2018-08-20 00:45:20 +0430

    add static podcasts assets

---
+++ static/podcasts/index.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id:1,
+        totalCount:1,
+        fieldValue:"Jigsaw",
+        title:"Jigsaw Podcast",
+        by:[
+            {
+                name: "Mehrad Rousta",
+                link: "https://twitter.com/mehrad77"
+            },
+            {
+                name: "Saro Khezernezhad",
+                link: "https://twitter.com/khezer77"
+            }
+        ],
+        cover:"https://alterview.xyz/podcasts/jigsaw/covers/episode002_1400x1400.jpg",
+        describtion: "this is a long describtion",
+        listen_on:[
+            {
+                name:"iTuens",
+                link:"https://example.com"
+            },{
+                name:"CastBox",
+                link:"https://example.com"
+            },{
+                name:"Sticher",
+                link:"https://example.com"
+            },{
+                name:"GooglePodcast",
+                link:"https://example.com"
+            },{
+                name:"RSS",
+                link:"https://example.com"
+            }
+        ]
+    }
+];
