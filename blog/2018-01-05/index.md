7422c46b5e82846274ed6f923174ca34a715fcba
Mehrad Rousta <mehrad77@gmail.com>
2018-08-19 06:00:51 +0430

    init

---
+++ blog/2018-01-05/index.md
@@ -0,0 +1,62 @@
+---
+date: "2018-01-05"
+title: "Coding Is Cool"
+category: "کد"
+---
+
+لسی لسیل سیل یستسیدتل ثصغت البس ابک ج ا قا س. لسی لسین اقصخه اقالدبس ابساقاا. سباقع ساخل تاس البیا بی؟ اسبقا تاد تعداعقداس اه سا سهاهبت اهدا به.  تیسالتسیادسنبدابنتس ان اق انبدا تمسبدبتد خسدا.
+
+یسلات ا دذابثخهتداخداخبدا.
+
+```js
+(function() {
+
+var cache = {};
+var form = $('form');
+var minified = true;
+
+var dependencies = {};
+
+var treeURL = 'https://api.github.com/repos/PrismJS/prism/git/trees/gh-pages?recursive=1';
+var treePromise = new Promise(function(resolve) {
+	$u.xhr({
+		url: treeURL,
+		callback: function(xhr) {
+			if (xhr.status < 400) {
+				resolve(JSON.parse(xhr.responseText).tree);
+			}
+		}
+	});
+});
+```
+
+Zwei flinke Boxer jagen die quir
+
+```md
+# asdfasdfads
+- auesufuaus
+```
+
+```css
+code[class*="language-"],
+pre[class*="language-"] {
+	color: black;
+	background: none;
+	font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
+	text-align: left;
+	white-space: pre;
+	word-spacing: normal;
+	word-break: normal;
+	word-wrap: normal;
+	line-height: 1.5;
+
+	-moz-tab-size: 4;
+	-o-tab-size: 4;
+	tab-size: 4;
+
+	-webkit-hyphens: none;
+	-moz-hyphens: none;
+	-ms-hyphens: none;
+	hyphens: none;
+}
+```
---
+++ config/SiteConfig.js
@@ -0,0 +1,25 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Alterview Podcasts', // Navigation and Site Title
+  siteTitleAlt: 'Alterview Podcasts - پادکست‌های آلترویو', // Alternative Site title for SEO
+  siteUrl: 'https://alterview.xyz/podcasts', // Domain of your site. No trailing slash!
+  siteLanguage: 'fa', // Language Tag on <html> element
+  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
+  siteDescription: 'پادکست‌های تولیدی توسط تیم آلترویو را از اینجا بشنوید!', // Your site description
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@alterviewxyz', // Twitter Username - Optional
+  ogSiteName: 'alterviewxyz', // Facebook Site Name - Optional
+
+  // Manifest and Progress color
+  // See: https://developers.google.com/web/fundamentals/web-app-manifest/
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Settings for typography.js
+  headerFontFamily: 'Bitter',
+  bodyFontFamily: 'Open Sans',
+  baseFontSize: '18px',
+};
